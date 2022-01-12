d74276031b225e8b2810d5e88cec5aa9c3ad0bc2
Park SungEun <77426806+tjddms5936@users.noreply.github.com>
2022-01-12 21:21:40 +0900

    Studying Character class

--- Source/FirstProject_cpp/Main.cpp
+++ Source/FirstProject_cpp/Main.cpp
@@ -4,7 +4,12 @@
 #include "Main.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "GameFramework/PlayerController.h"
 #include "Components/CapsuleComponent.h"
+#include "Engine/World.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
 // Sets default values
 AMain::AMain()
 {
@@ -25,6 +30,26 @@ AMain::AMain()
 	// Attach the camera to the end of the boom and let the boom adjust to match
 	// the controller orientation
 	FollowCamera->bUsePawnControlRotation = false;
+	// Set our turn rates for input
+	BaseTurnRate = 65.f;
+	BaseLookUpRate = 65.f;
+
+	// We don't want to rotate the character along with the rotation
+	// Let that just affect the camera.
+	// If you want to rotate your character along with camera, All of this value need to change for true.
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationPitch = false;
+
+	// 방향키 누르면 그 방향으로 캐릭터가 봄
+	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.f, 0.0f); // ... at this rotation rate // 얼마나 빠르게 방향회전하는지
+	GetCharacterMovement()->JumpZVelocity = 650.f; // Jump 높이 
+	GetCharacterMovement()->AirControl = 0.2f; // Character can moving in the air 
+
+
+
 }
 
 // Called when the game starts or when spawned
@@ -45,6 +70,54 @@ void AMain::Tick(float DeltaTime)
 void AMain::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	check(PlayerInputComponent); // PlayerInputComponent가 false라면 코드 멈춤
+
+	// 점프
+	// IE_Pressed : 눌렀을 때
+	// IE_Released : 땟을 때
+	// ACharacter를 이미 상속했고 그 안에는 Jump가 구현되어 있음.
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMain::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMain::MoveRight);
+
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("TurnRate", this, &AMain::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &AMain::LookUpAtRate);
 
 }
 
+
+void AMain::MoveForward(float Value) {
+	if (Controller != nullptr && Value != 0.0f) {
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+		// 카메라 전환되면 자연스럽게 캐릭터도 그 방향 봄
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMain::MoveRight(float Value) {
+	if (Controller != nullptr && Value != 0.0f) {
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMain::TurnAtRate(float Rate) {
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AMain::LookUpAtRate(float Rate) {
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
--- Source/FirstProject_cpp/Main.h
+++ Source/FirstProject_cpp/Main.h
@@ -23,6 +23,14 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 		class UCameraComponent* FollowCamera;
 
+	/** Base turn rates to scale turning functions for the camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	float BaseTurnRate;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	float BaseLookUpRate;
+
+
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -34,4 +42,21 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	/** Called for forwards/backwards input */
+	void MoveForward(float Value);
+	
+	/** Called for side to side input */
+	void MoveRight(float Value);
+
+	/** Called via input to turn at a given rate 
+	* @param Rate This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	*/
+	void TurnAtRate(float Rate);
+	/** Called via input to loot up/down at a given rate
+	* @param Rate This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	*/
+	void LookUpAtRate(float Rate);
+
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 };
